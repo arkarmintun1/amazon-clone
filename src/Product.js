b59f57d998cb2f3ad536223f8253221da12184a2
Arkar Min Tun <arkarmintun1@gmail.com>
2020-09-09 04:41:14 +0630

    coding along with sonny

---
+++ src/Product.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Product.css";
+import { useStateValue } from "./StateProvider";
+
+function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+  const addToBasket = () => {
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: { id, title, price, rating, image },
+    });
+  };
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <small>
+            $<strong>{price}</strong>
+          </small>
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>🌟</p>
+            ))}
+        </div>
+      </div>
+      <img src={image} alt="" />
+      <button onClick={addToBasket}>Add to Basket</button>
+    </div>
+  );
+}
+
+export default Product;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDUkS6LxUo271ljdQbXmrsSkkWGYwohT5o",
+  authDomain: "clone-7bed9.firebaseapp.com",
+  databaseURL: "https://clone-7bed9.firebaseio.com",
+  projectId: "clone-7bed9",
+  storageBucket: "clone-7bed9.appspot.com",
+  messagingSenderId: "1022409325684",
+  appId: "1:1022409325684:web:a319d671ba521da2116521",
+};
